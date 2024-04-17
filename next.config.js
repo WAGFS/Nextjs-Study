b72ee3641de74109597a4d5ea54986d97662b855
梁超 <1399566071@qq.com>
2024-04-17 15:40:20 +0800

    feat: Tailwind尝鲜

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Mr. Chao
+ * @Date: 2024-04-17 15:05:02
+ * @LastEditors: Mr. Chao
+ * @LastEditTime: 2024-04-17 15:29:41
+ * @Description: 
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
