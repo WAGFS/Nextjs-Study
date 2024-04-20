480a9293598d3f8a1b090bd406058666cb26f824
梁超 <1399566071@qq.com>
2024-04-20 17:20:50 +0800

    feat: 新增vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@
  * @Author: Mr. Chao
  * @Date: 2024-04-17 15:05:02
  * @LastEditors: Mr. Chao
- * @LastEditTime: 2024-04-17 15:55:05
+ * @LastEditTime: 2024-04-20 17:20:12
  * @Description: 
  */
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -16,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* 这里还添加了 Tailwind 的 antialiased 类，该类可使字体更加平滑。使用这个类并不是必需的，但它会增添一些美感。 */}
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
