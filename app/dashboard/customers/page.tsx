57e74f8c74c2af679467a799d948ab50121e5cca
CryptoCode16 <1399566071@qq.com>
2025-02-08 17:33:06 +0800

    feat: 使用Metadata添加页面元数据

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -5,6 +5,13 @@
  * @LastEditTime: 2024-04-17 19:00:33
  * @Description: 
  */
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title:'Customers'
+} 
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
